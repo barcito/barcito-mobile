879e607c3e64a3cd494f615d178acbe285b471ab
Raam4 <ramiro_cardozo22@hotmail.com>
2022-11-16 20:30:54 -0300

    new dependencies

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import 'react-native-gesture-handler';
 import AuthNavigator from './src/navigation/Auth.Navigator';
 import MainNavigator from './src/navigation/Main.Navigator';
+import 'react-native-gesture-handler';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
+      <StatusBar style="auto" />
       <Stack.Navigator>
         <Stack.Screen name="Auth" component={AuthNavigator} options={{ headerShown: false }} />
         <Stack.Screen name="Main" component={MainNavigator} options={{ headerShown: false }} />
       </Stack.Navigator>
-      <StatusBar style="auto" />
     </NavigationContainer>
   );
 }
