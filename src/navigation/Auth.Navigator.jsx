c2babda1ed4f4346c26a7536ecacbae44cca4f6e
Raam4 <ramiro_cardozo22@hotmail.com>
2022-11-18 20:19:37 -0300

    logic for welcome screen

--- src/navigation/Auth.Navigator.jsx
+++ src/navigation/Auth.Navigator.jsx
@@ -1,18 +1,44 @@
+import { useState, useEffect } from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import Welcome from "../screens/auth/Welcome";
 import Login from '../screens/auth/Login';
 import Register from "../screens/auth/Register";
-import Welcome from "../screens/auth/Welcome";
 
 const Stack = createNativeStackNavigator();
 
 const AuthNavigator = () => {
+
+    const [firstLaunch, setFirstLaunch] = useState(null);
+
+    useEffect(() => {
+      async function setData() {
+        /* await AsyncStorage.removeItem("appLaunched"); */
+        const appData = await AsyncStorage.getItem("appLaunched");
+        if (appData === null) {
+          setFirstLaunch(true);
+          await AsyncStorage.setItem("appLaunched", "false");
+        } else {
+          setFirstLaunch(false);
+        }
+      }
+      setData();
+    }, []);
     
     return(
-        <Stack.Navigator>
-            <Stack.Screen name="Login" component={Login} />
-            <Stack.Screen name="Register" component={Register} />
-            <Stack.Screen name="Welcome" component={Welcome} />
-        </Stack.Navigator>
+        firstLaunch != null && (
+            <Stack.Navigator>
+                {firstLaunch && (
+                    <Stack.Screen
+                        name="Welcome"
+                        component={Welcome}
+                        options={{ headerShown: false }}
+                    />
+                )}
+                <Stack.Screen name="Login" component={Login} />
+                <Stack.Screen name="Register" component={Register} />
+            </Stack.Navigator>
+        )
     );
 }
 
