7930c4103b380166fe104b47ae165d2c09950347
Raam4 <ramiro_cardozo22@hotmail.com>
2022-11-18 20:19:16 -0300

    custom toast

---
+++ src/components/atoms/CustomToast.jsx
@@ -0,0 +1,15 @@
+import Toast from "react-native-root-toast";
+
+const CustomToast = (message) => {
+    Toast.show(message, {
+        duration: Toast.durations.SHORT,
+        position: Toast.positions.BOTTOM,
+        shadow: true,
+        animation: true,
+        hideOnPress: true,
+        delay: 0,
+        textColor: 'red'
+    });
+}
+
+export default CustomToast;
